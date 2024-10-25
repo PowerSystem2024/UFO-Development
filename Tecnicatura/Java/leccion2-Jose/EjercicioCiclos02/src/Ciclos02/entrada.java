c035f9af954ff543783cf06df64586fa6e0df7b3
Jose Gomez <josepilar3@gmail.com>
2024-10-24 23:53:48 -0300

    Agrego  Leccion 2, 3 y 4

---
+++ Tecnicatura/Java/leccion2-Jose/EjercicioCiclos02/src/Ciclos02/entrada.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ciclos02;
+
+/**
+ *
+ * @author Jose
+ */
+class entrada {
+
+    static String nextLine() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
