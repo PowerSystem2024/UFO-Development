d6e77cdaa7a240c2e55c9f42bb88a4915777421a
Grupo14Ufo-Development <29934398@alumnos.frsr.utn.edu.ar>
2024-10-25 23:33:41 -0300

    Se completan ejercicios de ciclo while, do while y for

--- Tecnicatura/Java/Leccion2/CicloWhile/src/CicloWhile/EjercicioWhile01.java
+++ Tecnicatura/Java/Leccion2/CicloWhile/src/CicloWhile/EjercicioWhile01.java
@@ -5,7 +5,9 @@ package CicloWhile;
 public class EjercicioWhile01 {
     public static void main(String[] args) {
         
+        System.out.println("");
         System.out.println("Ciclo While");
+        
         var conteo = 0; // Inferencia de tipos
         while(conteo < 7){
             System.out.println("conteo = " + conteo);
@@ -14,10 +16,21 @@ public class EjercicioWhile01 {
         
         System.out.println("");
         System.out.println("Ciclo Do While");
+        
         var contador = 0;
         do {
             System.out.println("contador = " + contador);
             contador ++;
         } while( contador < 7);
+        
+        System.out.println("");
+        System.out.println("Ciclo for");
+        
+        for( var contando = 0; contando < 7; contando++) {
+            System.out.println("contando = " + contando);
+        } // En el ciclo for si hace una sola linea de codigo, se puede
+          // quitar las {} pero conviene dejarlas por buena práctica!
+          
+        
   }
 }
