bd254c78691397b461660405e9134f77b70c7f18
Roberto Jose Lopez <Roberlopez1105@gmail.com>
2024-10-28 10:19:11 -0300

    upload

--- Tecnicatura/Python/Leccion4/Clase01-Colecciones-Parte1/1-Ejercicios-listas/ejercicio1.py
+++
@@ -1,7 +0,0 @@
-print('iterar un rango de 0 a 10 e inprimir números divisibles entre 3')
-
-array = []
-for i in range(10):         # i es la variable y el rango 10 es el total de valores
-    if i % 3 == 0:          # valores divisibles entre 3
-        array.append(i)     # agregar al final del array
-print(array)                # imprimir el array imprime [0, 3, 6, 9]
--- Tecnicatura/Python/Leccion4/Clase01-Colecciones-Parte1/1-Ejercicios-listas/ejercicio2.py
+++
@@ -1,6 +0,0 @@
-print('Crear un rango de números del 2 al 6 e imprimirlos')
-
-numeros = []            # Creamos un array vacio
-for i in range(2,7):    # i es la variable que toma cada valor del rango que tiene inicio 2 y fin 7
-    numeros.append(i)   # se agregan de a uno los valores a la lista, el segundo valor y en adelante van al final
-print(numeros)          # imprimimos los numeros
