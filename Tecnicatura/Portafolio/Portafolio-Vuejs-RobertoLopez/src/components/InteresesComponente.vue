12bfab806fb747d13a14b6422510c046d8380edd
Roberto Jose Lopez <RoberLopez1105@gmail.com>
2024-11-09 21:59:54 -0300

    recocando detalles

--- Tecnicatura/Portafolio/Portafolio-Vuejs-RobertoLopez/src/components/InteresesComponente.vue
+++ Tecnicatura/Portafolio/Portafolio-Vuejs-RobertoLopez/src/components/InteresesComponente.vue
@@ -3,10 +3,10 @@ import { ref } from 'vue';
 import FondoLava from './FondoLava.vue';
 //Este es un arreglo con ref para que se pueda reactivar el cambio de los intereses
 const intereses = ref([
-    'Desarrollo de Software de Código Abierto: Contribuyo a proyectos en GitHub, colaborando con otros desarrolladores para mejorar herramientas y librerías populares.',
-    'Deportes al Aire Libre: Disfruto del senderismo y el ciclismo de montaña, actividades que practico regularmente para mantener un equilibrio entre el trabajo y la vida personal.',
-    'Tecnología de Innovación: Me apasiona explorar nuevas tendencias tecnológicas como la inteligencia artificial y el desarrollo de aplicaciones móviles.',
-    'Fotografía: Practico la fotografía como un hobby, lo que me ayuda a mantener una perspectiva creativa tanto en mi vida profesional como personal.'
+    'Desarrollo de Software y Programación: Comencé a estudiar programación en 2024 en la UTN San Rafael. Me apasiona la programación y me esfuerzo por aprender y mejorar mis habilidades en este campo.',
+    'Deportes y Actividades al Aire Libre: Disfruto de los deportes como el pádel y el fútbol, que practico regularmente para mantenerme activo. También me gustan las actividades al aire libre, como acampar y pescar, que me permiten desconectar y disfrutar de la naturaleza',
+    'Tecnología e Innovación: Me interesa explorar nuevas tendencias tecnológicas que amplíen mis conocimientos, especialmente en áreas relacionadas con la programación y el desarrollo de aplicaciones',
+    'Viajes y Tiempo con Familia y Amigos: Me encanta viajar y explorar nuevos lugares. También valoro mucho el tiempo que paso con mi familia y amigos, disfrutando de momentos juntos.'
 ]);
 </script>
 
