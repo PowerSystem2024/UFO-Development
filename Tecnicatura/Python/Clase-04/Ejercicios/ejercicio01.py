e147b70f0393777f07201b55eb5771ae33272925
Dario Benitez <dbenitez.prg@gmail.com>
2024-11-15 08:31:51 -0300

    Agrego algunos ejercicios extras a carpetas de Python

--- Tecnicatura/Python/Clase-04/Ejercicios/ejercicio01.py
+++ Tecnicatura/Python/Clase-04/Ejercicios/ejercicio01.py
@@ -4,5 +4,9 @@
 
 #Creamos la lista
 lista = [1, 2, 2, 3, 4, 4, 5, "Leandro", 1, 2, 2, 3, 4, 4, 5, "Leandro"]
-lista = list(set(lista)) #Convertimos la lista a un conjunto de tipo "set"
+# a continuación convertimos la lista a tipo set(conjunto) ya que un conjunto de este tipo no almacena elementos
+# repetidos.
+lista = list(set(lista)) # Convertimos la lista a un conjunto de tipo "set"
 print(lista) #Mostramos la lista
+# se convierte la lista a tipo set y luego se convierte en tipo lista nuevamente en un solo paso, 
+# es por eso que englobamos con list a todo el parentesis en donde convertimos la lista a un tipo set
